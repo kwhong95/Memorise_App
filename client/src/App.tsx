b862e5f876c091efb3d24e09563884d864e67fa8
kwhong95 <kwhong95@kakao.com>
2021-01-14 23:54:37 +0900

    Creating JSX Structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
 import React, { FC } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './asserts/memories.png';
+import Posts from './components/Posts/Post/Posts';
+import Form from './components/Form/Form';
 
 const App: FC = () => {
   return (
-    <div className="App">
-      Hello~
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memorires" height="300" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" align-items="stretch" spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Form/Form.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const Form: FC = () => {
+  return (
+    <div>
+      <h1>Form</h1>
+    </div>
+  )
+}
+
+export default Form;

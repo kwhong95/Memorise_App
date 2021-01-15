28aaabea77fe4e05ce8c3041248ea991869a6dab
kwhong95 <kwhong95@kakao.com>
2021-01-15 11:19:13 +0900

    Adding Styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './asserts/memories.png';
-import Posts from './components/Posts/Post/Posts';
+import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
+import useStyles from './styles';
+
 const App: FC = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memorires" height="300" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memorires" height="60" />
       </AppBar>
       <Grow in>
         <Container>
---
+++ client/src/components/Posts/Posts.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import Post from './Post/Post';
+
+const Posts: FC = () => {
+  return (
+    <>
+      <h1>
+        Posts
+      </h1>
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Posts;
